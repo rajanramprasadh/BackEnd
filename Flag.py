2c893017c222bc68711c7fa2a9d179f324228de5
rajanramprasadh <rajanramprasadh@gmail.com>
2016-02-16 21:59:31 +0530

    Create Flag.py

---
+++ Flag.py
@@ -0,0 +1,11 @@
+import json
+import firebase
+class Flag:
+	def __init__(self):
+		self.birthday=''
+	def flag(*data, *a)
+		firebase=firebase.FirebaseApplication('https://ramprasadhsample.firebaseio.com/', None)
+		json_data=data['birthdate']
+		for email in a['email']
+	result=firebase.put('/flag', json_data, {email['email']:'Sent'})
+	print result
